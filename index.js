80653fe79bebadaf5baa2911a893c73795f27979
rubanNg <ruban.mail1991@gmail.com>
2022-09-24 00:31:43 +0200

    srcx

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port =  process.env.NODE_ENV === "production" ? (process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
   readFile(resolve(__dirname, "./static/index.html"),  (c, t) => {
-    let html = t.toString().replace(/{{src}}/gi, req?.query?.src || "//57.annacdn.cc/DLhqnGFR1GmS/tv-series/13302");
+    let html = t.toString().replace(/{{src}}/gi, req?.query?.src || "//57.annacdn.cc/DLhqnGFR1GmS/movie/7064");
     res.setHeader("content-type", "text/html");
     res.send(html);
   })
